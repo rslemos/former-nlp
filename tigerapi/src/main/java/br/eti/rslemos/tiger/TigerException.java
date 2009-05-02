96f383ae9e14e1fdb890a9357f4fa2f2c6b046fc
R. Lemos <rslemos@gmail.com>
2009-05-02 15:22:56 +0000

    TigerException genérica, sem dependência com StAX

--- tigerapi/src/main/java/br/eti/rslemos/tiger/TigerException.java
+++ tigerapi/src/main/java/br/eti/rslemos/tiger/TigerException.java
@@ -1,12 +1,10 @@
 package br.eti.rslemos.tiger;
 
-import javax.xml.stream.XMLStreamException;
-
 public class TigerException extends Exception {
 
 	private static final long serialVersionUID = -1814827932980996458L;
 
-	public TigerException(String message, XMLStreamException cause) {
+	public TigerException(String message, Exception cause) {
 		super(message, cause);
 	}
 
