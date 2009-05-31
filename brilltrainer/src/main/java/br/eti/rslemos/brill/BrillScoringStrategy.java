be3d47104950f578e2512bb5f9048728d368c698
R. Lemos <rslemos@gmail.com>
2009-05-31 17:00:40 +0000

    refactoring para facilitar a captura de eventos (no módulo brillevents)

--- brilltrainer/src/main/java/br/eti/rslemos/brill/BrillScoringStrategy.java
+++ brilltrainer/src/main/java/br/eti/rslemos/brill/BrillScoringStrategy.java
@@ -21,21 +21,29 @@ public class BrillScoringStrategy implements ScoringStrategy {
 		int i = 0;
 		for (List<Token> proofSentence : trainingContext.proofCorpus) {
 			BufferingContext trainingSentence = trainingContext.trainingCorpus[i++];
-
-			try {
-				for (Token proofToken : proofSentence) {
-					trainingSentence.next();
-					
-					if (rule.matches(trainingSentence))
-						if (ObjectUtils.equals(rule.getTo(), proofToken.getTag()))
-							score++;
-						else
-							score--;
-				}
-			} finally {
-				trainingSentence.reset();
-			}
+			score += compute(rule, proofSentence, trainingSentence);
 		}
+		
 		return score;
+	}
+
+	private int compute(Rule rule, List<Token> proofSentence, BufferingContext trainingSentence) {
+		try {
+			int score = 0;
+			
+			for (Token proofToken : proofSentence) {
+				trainingSentence.next();
+				
+				if (rule.matches(trainingSentence))
+					if (ObjectUtils.equals(rule.getTo(), proofToken.getTag()))
+						score++;
+					else
+						score--;
+			}
+			
+			return score;
+		} finally {
+			trainingSentence.reset();
+		}
 	}
 }
