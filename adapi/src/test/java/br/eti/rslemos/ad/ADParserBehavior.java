9c36e8b57414a6284537cf5e1cc78a31e92be626
R. Lemos <rslemos@gmail.com>
2009-06-19 00:08:28 +0000

    removendo o ANTLR: o formato de árvores deitadas não é bom para parsing com ANTLR... na verdade o formato é bem line-oriented (e não token-oriented)

--- adapi/src/test/java/br/eti/rslemos/ad/ADParserBehavior.java
+++ adapi/src/test/java/br/eti/rslemos/ad/ADParserBehavior.java
@@ -2,8 +2,6 @@ package br.eti.rslemos.ad;
 
 import java.io.InputStream;
 
-import org.antlr.runtime.ANTLRInputStream;
-import org.antlr.runtime.CommonTokenStream;
 import org.testng.annotations.Test;
 
 public class ADParserBehavior {
@@ -11,10 +9,5 @@ public class ADParserBehavior {
 	@Test
 	public void shouldParseExt_1000() throws Throwable {
 		InputStream ext_1000 = ADParserBehavior.class.getResourceAsStream("ext_1000.ad");
-		ANTLRInputStream input = new ANTLRInputStream(ext_1000);
-        ADLexer lexer = new ADLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ADParser parser = new ADParser(tokens);
-        parser.arquivo();
 	}
 }
