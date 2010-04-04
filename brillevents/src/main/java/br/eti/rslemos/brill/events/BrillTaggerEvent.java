73085c0723bdeba734973bae3eba7524c317dd65
R. Lemos <rslemos@gmail.com>
2010-04-04 13:39:26 +0000

    equals e hashCode não eram mais usados

--- brillevents/src/main/java/br/eti/rslemos/brill/events/BrillTaggerEvent.java
+++ brillevents/src/main/java/br/eti/rslemos/brill/events/BrillTaggerEvent.java
@@ -64,44 +64,6 @@ public class BrillTaggerEvent extends EventObject implements Cloneable {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-
-		if (o == null)
-			return false;
-
-		if (this.getClass() != o.getClass())
-			return false;
-
-		BrillTaggerEvent other = (BrillTaggerEvent)o;
-		
-		return
-			(source != null ? source.equals(other.source) : other.source == null) &&
-			(onSentence != null ? onSentence.equals(other.onSentence) : other.onSentence == null) &&
-			(actingRule != null ? actingRule.equals(other.actingRule) : other.actingRule == null) &&
-			(atContext != null ? atContext.equals(other.atContext) : other.atContext == null) &&
-			(currentToken != null ? currentToken.equals(other.currentToken) : other.currentToken == null);
-	}
-
-	@Override
-	public int hashCode() {
-		int hashCode = 0;
-		
-		hashCode += source != null ? source.hashCode() : 0;
-		hashCode *= 3;
-		hashCode += onSentence != null ? onSentence.hashCode() : 0;
-		hashCode *= 5;
-		hashCode += actingRule != null ? actingRule.hashCode() : 0;
-		hashCode *= 7;
-		hashCode += atContext != null ? atContext.hashCode() : 0;
-		hashCode *= 11;
-		hashCode += currentToken != null ? currentToken.hashCode() : 0;
-		
-		return hashCode;
-	}
-
-	@Override
 	public Object clone() {
 		try {
 			return super.clone();
