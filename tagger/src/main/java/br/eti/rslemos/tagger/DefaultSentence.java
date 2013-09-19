aa6658295b20c08e4712ee666630f2081a582303
R. Lemos <rslemos@gmail.com>
2013-09-19 08:37:58 -0300

    Sentence and Token concepts as simple List and Map
    DefaultSentence and DefaultToken are therefore simple
    extensions of ArrayList and HashMap.

--- tagger/src/main/java/br/eti/rslemos/tagger/DefaultSentence.java
+++ tagger/src/main/java/br/eti/rslemos/tagger/DefaultSentence.java
@@ -3,6 +3,7 @@
  * 
  * This file is part of program "Natural Language Processing"
  * Copyright 2011  Rodrigo Lemos
+ * Copyright 2013  Rodrigo Lemos
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -22,61 +23,21 @@
 package br.eti.rslemos.tagger;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
-public class DefaultSentence implements Sentence {
-	private final List<Token> sentence;
+public class DefaultSentence extends ArrayList<Token> implements Sentence {
 
-	public DefaultSentence(Token[] sentence) {
-		this(Arrays.asList(sentence));
-	}
-	
+	private static final long serialVersionUID = 3090188575586194869L;
+
 	public DefaultSentence(List<Token> sentence) {
-		this.sentence = sentence;
+		super(sentence);
 	}
 
 	public DefaultSentence(Sentence sentence) {
-		this.sentence = new ArrayList<Token>(sentence.size());
-		for (Token token : sentence) {
-			this.sentence.add(new DefaultToken(token));
-		}
-		
-		((ArrayList<Token>)this.sentence).trimToSize();
+		super(sentence);
 	}
 
-	public Iterator<Token> iterator() {
-		return sentence.iterator();
+	public DefaultSentence() {
+		super();
 	}
-
-	public int size() {
-		return sentence.size();
-	}
-
-	public Token get(int i) {
-		try {
-			return sentence.get(i);
-		} catch (IndexOutOfBoundsException e) {
-			throw new SentenceIndexOutOfBoundsException(i);
-		}
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder result = new StringBuilder();
-		result.append("\"");
-		
-		for (Token token : sentence) {
-			result.append(token.toString()).append(" ");
-		}
-		
-		result.setLength(result.length() - 1);
-		
-		result.append("\"");
-		
-		return result.toString();
-	}
-	
-	
 }
